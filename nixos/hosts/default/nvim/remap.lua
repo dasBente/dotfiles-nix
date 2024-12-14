6c18953dcfb02a6203e7c08609dba3adc1e5cf9a
dasBente <dasbente@gmail.com>
2024-12-14 15:02:53 +0100

    nvim: fix typo for line shift

--- nixos/hosts/default/nvim/remap.lua
+++ nixos/hosts/default/nvim/remap.lua
@@ -11,7 +11,7 @@ vim.keymap.set(
 
 -- move highlighted lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<.2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- append next line
 vim.keymap.set("n", "J", "mzJ`z")
