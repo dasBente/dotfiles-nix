fc25d3baa690f03f6a853b9eb98d8c24f5d842ec
dasBente <dasbente@gmail.com>
2025-01-21 18:37:45 +0000

    add: automated updates and garbage collection

--- hosts/common.nix
+++ hosts/common.nix
@@ -6,6 +6,20 @@
   config,
   ...
 }: {
+  # Update automatically
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.dates = "weekly";
+
+  # Automatic garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 14d";
+  };
+
+  nix.settings.auto-optimise-store = true;
+
+  # Write all installed packages to /etc/current-system-packages
   environment.etc."current-system-packages".text = let
     packages =
       builtins.map (p: "${p.name}") config.environment.systemPackages;
