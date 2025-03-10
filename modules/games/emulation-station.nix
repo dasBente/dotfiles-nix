6903f30bd41cf84dc431257e4c838e45e77ef117
dasBente <dasbente@gmail.com>
2025-03-10 17:07:12 +0100

    retroarch cores

--- modules/games/emulation-station.nix
+++ modules/games/emulation-station.nix
@@ -4,10 +4,19 @@
       lib.mkEnableOption "Enables Emulation Station Desktop";
 
   config = lib.mkIf config.games.emulation-station.enable {
-    environment.systemPackages = [
-      pkgs.emulationstation
-    ];
-    
+    environment.systemPackages = with pkgs; [
+      emulationstation
+
+      (retroarch.withCores (cores: with cores; [
+          bsnes
+          mgba
+          quicknes
+          beetle-psx-hw
+          mupen64plus
+          citra
+          dolphin
+        ]))
+      ];
   };
 }
 
